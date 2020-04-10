b936d33ee20ee6210290711105f30cc53b6a4d49
Natalia Rozhkova <kobra1789@gmail.com>
2020-04-10 21:47:45 +0500

    Create new repository

---
+++ app/src/main/java/com/example/nytimes/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.nytimes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return  true;
+    }
+
+    @Override
+
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        Intent intent = new Intent();
+        startActivity(new Intent(this, AboutAuthorActivity.class));
+        return true;
+    }
+
+}
